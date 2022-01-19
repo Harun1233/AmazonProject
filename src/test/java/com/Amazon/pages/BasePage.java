7c8e455098d56e542e62c2cb6c9695cebfa72643
Harun1233 <av.harunonat@gmail.com>
2022-01-19 10:06:58 +0000

    BasePage constructor has generated

--- src/test/java/com/Amazon/pages/BasePage.java
+++ src/test/java/com/Amazon/pages/BasePage.java
@@ -1,6 +1,11 @@
 package com.Amazon.pages;
 
+import com.Amazon.tests.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public abstract class BasePage {
+    public BasePage() {
+        PageFactory.initElements(Driver.get(),this);
 
-
+    }
 }
