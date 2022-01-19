086d4a2504ea3a483ac8d870645871d7cb5d14da
Harun1233 <av.harunonat@gmail.com>
2022-01-19 23:58:18 +0000

    FirstHatPage select and getPrice method added

--- src/test/java/com/Amazon/pages/FirstHatPage.java
+++ src/test/java/com/Amazon/pages/FirstHatPage.java
@@ -1,4 +1,20 @@
 package com.Amazon.pages;
 
-public class FirstHatPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class FirstHatPage extends BasePage {
+
+    @FindBy(xpath = "//div[@class='a-section a-spacing-base'][1]//div[3]//div//a//span//span[1]")
+    public WebElement firstHat;
+
+    public void selectFirstHat(){
+        firstHat.click();
+    }
+
+    @Override
+    public Double getPrices(WebElement element) {
+        element=firstHat;
+        return super.getPrices(element);
+    }
 }
