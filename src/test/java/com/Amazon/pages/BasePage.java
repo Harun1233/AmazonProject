da06e6deaada691b63ae22e8a5b6a97c7335b237
Harun1233 <av.harunonat@gmail.com>
2022-01-19 10:28:17 +0000

    BasePage is in proccess

--- src/test/java/com/Amazon/pages/BasePage.java
+++ src/test/java/com/Amazon/pages/BasePage.java
@@ -1,6 +1,8 @@
 package com.Amazon.pages;
 
 import com.Amazon.tests.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
@@ -8,4 +10,8 @@ public abstract class BasePage {
         PageFactory.initElements(Driver.get(),this);
 
     }
+    public void navigateTo(WebElement e){
+        e.click();
+    }
+
 }
