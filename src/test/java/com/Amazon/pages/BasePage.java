8e736a1bd090844d9ecd22e61fd400bf873e6b65
Harun1233 <av.harunonat@gmail.com>
2022-01-19 11:33:59 +0000

    BasePage dorpDowns and getPrice methods added

--- src/test/java/com/Amazon/pages/BasePage.java
+++ src/test/java/com/Amazon/pages/BasePage.java
@@ -4,8 +4,12 @@ import com.Amazon.tests.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public abstract class BasePage {
+
+    Select select;
+
     public BasePage() {
         PageFactory.initElements(Driver.get(),this);
 
@@ -13,5 +17,31 @@ public abstract class BasePage {
     public void navigateTo(WebElement e){
         e.click();
     }
+
+    //selects quantities and returns selected quantity
+    public String dropDowns(WebElement e, String str) {
+        select = new Select(e);
+        select.selectByVisibleText(str);
+
+        return e.getText();
+    }
+
+    public Double getPrices(String str){
+
+        double priceOfItems=0.0;
+        try{
+            String ItemPrice = Driver.get().findElement(By.xpath(str)).getText();
+            ItemPrice.split("$");
+             priceOfItems=Double.parseDouble(ItemPrice);
+
+        }catch(Exception e){
+            e.getLocalizedMessage();
+            System.out.println("Getting prices did not work");
+        }
+
+        return priceOfItems;
+    }
+
+
 
 }
