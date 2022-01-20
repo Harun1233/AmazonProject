7ca625d2ce529e94a1a96e2a667fb39779c983d1
Harun1233 <av.harunonat@gmail.com>
2022-01-20 18:49:55 +0000

    getTextOfCartQuantity and changeQuantityOfCart methods has been added

--- src/test/java/com/Amazon/pages/CartPage.java
+++ src/test/java/com/Amazon/pages/CartPage.java
@@ -1,9 +1,12 @@
 package com.Amazon.pages;
 
+import com.Amazon.tests.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class CartPage extends BasePage{
+    Select select;
 
     @FindBy(css = "#sw-gtc")
     public static WebElement cartButton;
@@ -14,5 +17,20 @@ public class CartPage extends BasePage{
     @FindBy(xpath = "//*[@id='sc-subtotal-amount-buybox']//span")
     public static WebElement totalPriceOfCart;
 
+    public String getTextOfCartQuantity(WebElement element){
+
+        select=new Select(element);
+
+        return select.getFirstSelectedOption().getText();
+
+    }
+
+    public void changeQuantityOfCart(WebElement element,String str){
+
+        select=new Select(element);
+
+        select.selectByVisibleText(str);
+
+    }
 
 }
