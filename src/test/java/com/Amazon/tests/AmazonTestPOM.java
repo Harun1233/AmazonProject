9575f268e8df54c833c3d75b1cb30d3d394d47fe
Harun1233 <av.harunonat@gmail.com>
2022-01-20 19:22:49 +0000

    Last modifications has been made

--- src/test/java/com/Amazon/tests/AmazonTestPOM.java
+++ src/test/java/com/Amazon/tests/AmazonTestPOM.java
@@ -21,24 +21,44 @@ public class AmazonTestPOM extends TestBase{
     public void test1(){
 
         quantityOfItem="2";
+
+        //Search for required item
         searchPage.setSearchBox();
+
+        //Select the first displayed product
         firstHat.selectFirstHat();
+
+        //Get the price of seelcted product
         priceOfHat= firstHat.getPrices(FirstHatPage.priceOfFirstHat);
+
+        //Get the total price after quantity changed
         totalPrice=firstHat.dropDowns(FirstHatPage.quantityDropDown,quantityOfItem)*priceOfHat;
+
+        //Add the product to the cart
         firstHat.addToCart();
+
+        //Go to the cart page
         cart.navigateTo(CartPage.cartButton);
+
+        //Get the displayed price on cart page
         Double cartPagePrices = cart.getPrices(CartPage.totalPriceOfCart);
+
         //Verify the total price is correct
         Assert.assertTrue(totalPrice==cartPagePrices,"Verify that the cart page prices matches with the correct amount");
+
+        //Get the quantity of products which is displayed on cart page
         quantityOfCart=cart.getTextOfCartQuantity(CartPage.quantityOfCart);
+
         //Verify that selected item quantity is correct
         Assert.assertEquals(quantityOfCart,quantityOfItem,"Verify that quantity of cart page is equal selected quantity");
 
+        //Change the qunatity on cart page
         quantityOfCart="1";
-
         totalPrice=cart.dropDowns(CartPage.quantityOfCart,quantityOfCart)*priceOfHat;
+
         //Verify that quantity in Cart page has been changed successfully
         Assert.assertEquals(quantityOfCart,cart.getTextOfCartQuantity(CartPage.quantityOfCart));
+
         //Verify that total price has been changed successfully
         Assert.assertTrue(totalPrice==priceOfHat,"Verify that total price has been changed");
 
