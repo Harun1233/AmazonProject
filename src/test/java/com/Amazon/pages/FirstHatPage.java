0531d5deb40533a6c7ca15ac025583c6b34945f8
Harun1233 <av.harunonat@gmail.com>
2022-01-20 00:24:05 +0000

    FirstHatPage getPrice method has been modified

--- src/test/java/com/Amazon/pages/FirstHatPage.java
+++ src/test/java/com/Amazon/pages/FirstHatPage.java
@@ -1,20 +1,26 @@
 package com.Amazon.pages;
 
+import com.Amazon.tests.utilities.BrowserUtils;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class FirstHatPage extends BasePage {
 
-    @FindBy(xpath = "//div[@class='a-section a-spacing-base'][1]//div[3]//div//a//span//span[1]")
-    public WebElement firstHat;
+
+    @FindBy(xpath = "//div[@class='a-section aok-relative s-image-square-aspect']")
+    public static WebElement firstHat;
+
+    @FindBy(xpath = "//span[@id='price_inside_buybox']")
+    public static WebElement priceOfFirstHat;
 
     public void selectFirstHat(){
+        BrowserUtils.waitForVisibility(firstHat,10);
         firstHat.click();
     }
 
     @Override
     public Double getPrices(WebElement element) {
-        element=firstHat;
+
         return super.getPrices(element);
     }
 }
